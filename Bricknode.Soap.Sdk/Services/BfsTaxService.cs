b7c2cf9378e41d1389364656dc71e5cd9c47b6f8
Andreas <andreas76grahn@hotmail.com>
2020-09-01 13:51:36 +0200

    Initial

---
+++ Bricknode.Soap.Sdk/Services/BfsTaxService.cs
@@ -0,0 +1,19 @@
+﻿using BfsApi;
+using Bricknode.Soap.Sdk.Configuration;
+using Bricknode.Soap.Sdk.Factories;
+using Bricknode.Soap.Sdk.Services.Bases;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Bricknode.Soap.Sdk.Services
+{
+    public class BfsTaxService : BfsServiceBase
+    {
+        private readonly bfsapiSoap _client;
+
+        public BfsTaxService(IOptions<BfsApiConfiguration> bfsApiConfiguration, ILogger logger, IBfsApiClientFactory bfsApiClientFactory, bfsapiSoap client) : base(bfsApiConfiguration, logger, bfsApiClientFactory, client)
+        {
+            _client = client;
+        }
+    }
+}
