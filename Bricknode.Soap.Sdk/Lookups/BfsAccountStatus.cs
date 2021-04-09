cca096329c38917bbcbaef4a13aff7de3978989e
olofalmqvist <olof.almqvist@untiegroup.se>
2021-04-09 15:37:47 +0200

    Minor change in comment.

--- Bricknode.Soap.Sdk/Lookups/BfsAccountStatus.cs
+++ Bricknode.Soap.Sdk/Lookups/BfsAccountStatus.cs
@@ -5,7 +5,7 @@
             public const int Open = 1;
             public const int Closed = 2;
             /// <summary>
-            /// This means that the account is hidden from users of the standard customer portal.
+            /// Hidden means that the account is hidden from users of the standard customer portal.
             /// </summary>
             public const int Hidden = 4;
         }
