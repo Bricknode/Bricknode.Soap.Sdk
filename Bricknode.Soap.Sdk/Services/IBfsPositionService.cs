15689b17819af3ced0ee5edcd8a9ebd1e5f7080c
Mathias Näss <mathias.nass@bricknode.com>
2025-09-24 14:03:29 +0200

    chore: getpositions paging

--- Bricknode.Soap.Sdk/Services/IBfsPositionService.cs
+++ Bricknode.Soap.Sdk/Services/IBfsPositionService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using BfsApi;
 
 namespace Bricknode.Soap.Sdk.Services
@@ -13,6 +14,17 @@ namespace Bricknode.Soap.Sdk.Services
         /// <returns></returns>
         Task<GetPositionResponse> GetPositionsAsync(GetPositionArgs filters, string? bfsApiClientName = null);
 
+        /// <summary>
+        /// https://bricknode.atlassian.net/wiki/spaces/API/pages/52002925/GetPositions
+        /// </summary>
+        /// <param name="filters"></param>
+        /// <param name="bfsApiClientName"></param>
+        /// <param name="pageSize">Size of each page, supported range is 1 to 5000. The default size is 2000.</param>
+        /// <param name="pageStartIndex">Index of the start page. Can be used to skip specific number of pages in enumeration.</param>
+        /// <returns></returns>
+        IAsyncEnumerable<GetPositionResponse> GetPositionsInPagesAsync(GetPositionArgs filters, int pageSize = 2000,
+            int pageStartIndex = 0, string? bfsApiClientName = null);
+        
         /// <summary>
         /// https://bricknode.atlassian.net/wiki/spaces/API/pages/60031196/GetHistoricPositions
         /// </summary>
